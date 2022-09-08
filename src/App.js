c4551a9a1303b3d9c3ff6fbe8767fe03277a22a5
Shivammohite32 <113103276+Shivammohite32@users.noreply.github.com>
2022-09-08 20:56:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className="App">
+      <h1>INTERNSHIP PROGRAM</h1>
+       <h3>This is the Home Page</h3>
+       
+      
+    </div>
+  )
+}
+
+export default App;
